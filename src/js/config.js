47d9f4dafea04b024bc02c0f392b0917d861e419
Dmitriy Angve <kirirch129@gmail.com>
2022-11-22 12:37:32 +0300

    #

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes'; // UpperCase because this a constant, will never change
+export const TIMEOUT_SEC = 10;
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,139 +1,38 @@
+import * as model from './model.js'; // "*" - all
+import recipeView from './views/recipeView.js';
+
+import 'core-js/stable'; // for polyfiling everything else
+import 'regenerator-runtime/runtime'; // for polyfiling async await
+
 const recipeContainer = document.querySelector('.recipe');
 // console.log(recipeContainer);
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
 
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
-const showRecipe = async function () {
-  try {
-    // 1) Loading recipe
-    const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcfb2'
-    );
-    const data = await res.json(); // Convert to json (call json on that response wich returns another promise which we then have to await again, and in the end we will get our data stored to that variable)
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
 
-    let { recipe } = data.data; // change object property name
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-    console.log(recipe);
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1); // change the hash id with slice 2 first element
+    // console.log(id);
+
+    // guard clause (if no id)
+    if (!id) return;
+    recipeView.renderSpinner(); // Render spinner
+
+    // 1) Loading recipe
+    await model.loadRecipe(id);
+    // async function and it is going to return a promise (here we have to avoid that promise before we can move on in the next step)
 
     // 2) Rendering recipe
-
-    const markup = `
-    <figure class="recipe__fig">
-          <img src="${recipe.image}" alt="${
-      recipe.title
-    }" class="recipe__img" />
-          <h1 class="recipe__title">
-            <span>${recipe.title}</span>
-          </h1>
-        </figure>
-        <div class="recipe__details">
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="src/img/icons.svg#icon-clock"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--minutes">${
-              recipe.cookingTime
-            }</span>
-            <span class="recipe__info-text">minutes</span>
-          </div>
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="src/img/icons.svg#icon-users"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--people">${
-              recipe.servings
-            }</span>
-            <span class="recipe__info-text">servings</span>
-            <div class="recipe__info-buttons">
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use href="src/img/icons.svg#icon-minus-circle"></use>
-                </svg>
-              </button>
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use href="src/img/icons.svg#icon-plus-circle"></use>
-                </svg>
-              </button>
-            </div>
-          </div>
-          <div class="recipe__user-generated">
-            <svg>
-              <use href="src/img/icons.svg#icon-user"></use>
-            </svg>
-          </div>
-          <button class="btn--round">
-            <svg class="">
-              <use href="src/img/icons.svg#icon-bookmark-fill"></use>
-            </svg>
-          </button>
-        </div>
-        <div class="recipe__ingredients">
-          <h2 class="heading--2">Recipe ingredients</h2>
-          <ul class="recipe__ingredient-list">
-          ${recipe.ingredients
-            .map(ing => {
-              return `
-            <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="src/img/icons.svg#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">${ing.quantity}</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">${ing.unit}</span>
-                ${ing.description}
-              </div>
-            </li>
-            `;
-            })
-            .join('')}           
-        </div>
-
-        <div class="recipe__directions">
-          <h2 class="heading--2">How to cook it</h2>
-          <p class="recipe__directions-text">
-            This recipe was carefully designed and tested by
-            <span class="recipe__publisher">${
-              recipe.publisher
-            }</span>. Please check out
-            directions at their website.
-          </p>
-          <a
-            class="btn--small recipe__btn"
-            href="${recipe.sourceUrl}"
-            target="_blank"
-          >
-            <span>Directions</span>
-            <svg class="search__icon">
-              <use href="src/img/icons.svg#icon-arrow-right"></use>
-            </svg>
-          </a>
-        </div>
-    `;
-    recipeContainer.innerHTML = ''; // remove message "Start by seacrching for a recipe..."
-    recipeContainer.insertAdjacentHTML('afterbegin', markup);
+    recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    console.log(err);
   }
 };
-showRecipe();
+
+// And function and data flow. As we start to programm then the init() runs and it will then immediately run addHandlerRender. AddHandlerRender listening for events in the "recipeView.js". As we call addHandlerRender we pass in the controller function (controlRecipes) / handler function that we want to get executed as soon as the event happens. Thenreceive that function as being called handler and so that's what we then call as soon as the event happens
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
